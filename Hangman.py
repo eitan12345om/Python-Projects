e0aa9c6999119732e1f786e2ce6dee0c2c8a9a15
eitan12345om <eitan.simler@gmail.com>
2014-11-12 08:56:44 -0800

    initial commit

---
+++ Hangman.py
@@ -0,0 +1,54 @@
+def replace(word, character, index):
+	try:
+		return word[:index] + character + word[index + 2:]
+	except IndexError:
+		return word[:index] + character
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+word = 'apple'
+Guessed_word = '_ ' * len(word)
+Lives = 5
+a = 0
+
+while Lives > 0 and a == 0:
+	
+	Valid_Guess = False
+	while Valid_Guess == False:
+		print('\n' + Guessed_word)
+		print('Available letters to guess:', alphabet)
+		
+		if Lives != 1:
+			print('You have', Lives, 'lives left')
+		else:
+			print('You have', Lives, 'life left')
+		
+		guess = input('Guess one character: ')
+		if guess.isalpha():
+			guess = guess.lower()
+		if guess in alphabet:
+			Valid_Guess = True
+		else:
+			print('Oops! Invalid Guess!')
+	
+	if guess in word:
+		index = 0
+		for ch in word:
+			if guess == ch:
+				Guessed_word = replace(Guessed_word, guess, index)
+			index += 1
+		print('Hooray! You guessed a correct letter!')
+	else:
+		if Lives != 1:
+			print('\n' + 'Try Again!')
+		Lives -= 1
+	
+	alphabet = alphabet.replace(guess, '')
+	if Guessed_word == word:
+		a = 1
+		print('\n' + word)
+
+if a == 1:
+	print('\n' + 'You Win!')
+else:
+	print('\n' + 'You Lose!')
+
