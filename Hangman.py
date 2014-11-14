3d7a112738a944a5098cfbb301ca84bf6aea92b1
eitan12345om <eitan.simler@gmail.com>
2014-11-13 20:01:20 -0800

    Added a play again option

--- Hangman.py
+++ Hangman.py
@@ -7,53 +7,67 @@ def replace(word, character, index):
 	except IndexError:
 		return word[:index] + character
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-words = ['jazz', 'refrigerator', 'beautiful', 'rhythm', 'bookworm', 'croquet', 'knapsack', 'mystify', 'glowworm', 'numbskull', 'oxygen', 'pizazz', 'rhubarb', 'pixel', 'unknown', 'zombie', 'vortex']
-word = random.choice(words)
-Guessed_word = '_' * len(word)
-Lives = 7
-a = 0
+Play_again = 'y'
 
-while Lives > 0 and a == 0:
-	
-	Valid_Guess = False
-	while Valid_Guess == False:
-		print('\n' + Guessed_word, '(' + str(len(word)) + ' characters long)')
-		print('Available letters to guess:', alphabet)
+while Play_again == 'y':
+	alphabet = 'abcdefghijklmnopqrstuvwxyz'
+	words = ['jazz', 'refrigerator', 'beautiful', 'rhythm', 'bookworm', 'croquet', 'knapsack', 'mystify', 'glowworm', 'numbskull', 'oxygen', 'pizazz', 'rhubarb', 'pixel', 'unknown', 'zombie', 'vortex']
+	word = random.choice(words)
+	Guessed_word = '_' * len(word)
+	Lives = 7
+	Win = 0
+
+	while Lives > 0 and Win == 0:
 		
-		if Lives != 1:
-			print('You have', Lives, 'lives left')
-		else:
-			print('You have', Lives, 'life left')
+		Valid_Guess = False
+		while Valid_Guess == False:
+			print('\n' + Guessed_word, '(' + str(len(word)) + ' characters long)')
+			print('Available letters to guess:', alphabet)
+			
+			if Lives != 1:
+				print('You have', Lives, 'lives left')
+			else:
+				print('You have', Lives, 'life left')
+			
+			guess = input('Guess one character: ')
+			if guess.isalpha():
+				guess = guess.lower()
+			if guess in alphabet:
+				Valid_Guess = True
+			else:
+				print('Oops! Invalid Guess!')
 		
-		guess = input('Guess one character: ')
-		if guess.isalpha():
-			guess = guess.lower()
-		if guess in alphabet:
-			Valid_Guess = True
+		if guess in word:
+			index = 0
+			for ch in word:
+				if guess == ch:
+					Guessed_word = replace(Guessed_word, guess, index)
+				index += 1
+			print('\n' + 'Hooray! You guessed a correct letter!')
 		else:
-			print('Oops! Invalid Guess!')
-	
-	if guess in word:
-		index = 0
-		for ch in word:
-			if guess == ch:
-				Guessed_word = replace(Guessed_word, guess, index)
-			index += 1
-		print('\n' + 'Hooray! You guessed a correct letter!')
+			if Lives != 1:
+				print('\n' + 'Try Again!')
+			Lives -= 1
+		
+		alphabet = alphabet.replace(guess, '')
+		if Guessed_word == word:
+			Win = 1
+			print('\n' + word)
+
+	if Win == 1:
+		print('\n' + 'You Win!')
 	else:
-		if Lives != 1:
-			print('\n' + 'Try Again!')
-		Lives -= 1
-	
-	alphabet = alphabet.replace(guess, '')
-	if Guessed_word == word:
-		a = 1
-		print('\n' + word)
+		print('\n' + 'You Lose!')
+		print('The word was:', word)
 
-if a == 1:
-	print('\n' + 'You Win!')
-else:
-	print('\n' + 'You Lose!')
-	print('The word was:', word)
-
+	while Valid_Guess == True:
+		print('\n')
+		Play_again = input('Do you want to play again (y/n)? ')
+		if Play_again == 'y':
+			print('Good luck!')
+			Valid_Guess = False
+		elif Play_again == 'n':
+			print('See you next time!')
+			Valid_Guess = False
+		else:
+			print("Please enter either 'y' or 'n'")
